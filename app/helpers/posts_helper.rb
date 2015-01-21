5d896ea58f7e89c2dc7f655a51e502285d4cf3fc
Przemek <przemek@krowinski.eu>
2015-01-21 05:53:20 +0100

    Add image relation to post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def images_select_options
+    images_array = Image.all.map { |image| [image.name, image.id] }
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_one :image
+
   acts_as_taggable
+  
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
