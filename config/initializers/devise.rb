f207e48f4c758d95256bb898012f917ace283286
Przemek <przemek@krowinski.eu>
2015-01-21 03:39:08 +0100

    Add file-based secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
-  config.secret_key = File.read("shared/secrets/secret_key") if Rails.env.production?
+  config.secret_key = File.read("../shared/secrets/secret_key") if Rails.env.production?
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
