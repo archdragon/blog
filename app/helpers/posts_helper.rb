24a30cf45c256b2ae177b8e190593f098af3b269
Przemek <przemek@krowinski.eu>
2015-01-21 06:24:00 +0100

    Add helper for getting list of images

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
   def images_select_options
-    images_array = Image.all.map { |image| [image.name, image.id] }
+    Image.all.map { |image| [image.name, image.id] }
   end
 end
