d1662bcbdacd4311239b0570d2c414633709daf4
Przemek <przemek@krowinski.eu>
2015-01-24 23:05:02 +0100

    Apply published scope for public sections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, only: [:create, :upate, :destroy, :edit, :new]
 
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.published.all
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.published.find(params[:id])
+    set_page_head_data(@post)
     render layout: "single_post"
   end
 
@@ -59,8 +61,12 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.friendly.find(params[:id])
-      set_page_title(@post.title)
-      set_page_description(@post.tagline)
+      set_page_head_data(@post)
+    end
+
+    def set_page_head_data(post)
+      set_page_title(post.title)
+      set_page_description(post.tagline)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
