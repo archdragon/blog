930df06355c38fad3db1423d0a188c65f9116a4c
Przemek <przemek@krowinski.eu>
2015-05-16 21:10:07 +0200

    Add presenters, remove default comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, only: [:create, :upate, :destroy, :edit, :new, :unpublished]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.published.all
   end
@@ -13,30 +10,28 @@ class PostsController < ApplicationController
     render :index
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
     if user_signed_in?
       @post = Post.friendly.find(params[:id])
     else
       @post = Post.published.friendly.find(params[:id])
     end
-    
+
+    @post = present(@post)
+
     set_page_head_data(@post)
     render layout: "single_post"
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
+    post = Post.friendly.find(params[:id])
+    @post = present(post)
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
     @post.user = current_user
@@ -49,8 +44,6 @@ class PostsController < ApplicationController
 
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     @post.user = current_user
     if @post.update(post_params)
@@ -60,20 +53,12 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     redirect_to posts_url, notice: 'Post was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.friendly.find(params[:id])
-      set_page_head_data(@post)
-    end
-
     def set_page_head_data(post)
       set_page_title(post.title)
       set_page_description(post.tagline)
