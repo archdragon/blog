ab731128c8d238924dd171ba3318ddb966d8bd2d
Przemek <przemek@krowinski.eu>
2015-05-16 21:04:45 +0200

    Move markdown formatting to the Post presenter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,4 @@
 module ApplicationHelper
-  def markdown(text)
-    options = {
-      filter_html:     false,
-      hard_wrap:       true, 
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true, 
-      fenced_code_blocks: true
-    }
-
-    extensions = {
-      autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true,
-      fenced_code_blocks: true,
-      tables: true
-    }
-
-    renderer = Rouge::Renderer::HTML5.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
-
-    html_text = markdown.render(text).html_safe
-  end
-
   def page_title
     @page_title ? @page_title + " - " + Rails.application.config.default_page_title : Rails.application.config.default_page_title
   end
@@ -33,4 +10,5 @@ module ApplicationHelper
   def page_description
     @page_description || Rails.application.config.default_page_description
   end
+
 end
